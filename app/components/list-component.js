0b4dc2af921aba9e5036f662d96b3a12ced730a1
Michael Dao <michael.dao@gmail.com>
2015-05-05 15:25:32 -0600

    Update litsts

--- app/components/list-component.js
+++ app/components/list-component.js
@@ -1,9 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editing: false,
   actions: {
-    delete: function () {
-      this.get('list').destroyRecord();
-    }
+    edit: function () {
+      let editing = this.get('editing');
+      if (editing) {
+        this.get('list').save().then(() => {
+          this.set('editing', false);
+        });
+      } else {
+        this.set('editing', true);
+      }
+    },
+  delete: function () {
+    this.get('list').destroyRecord();
   }
+}
 });
